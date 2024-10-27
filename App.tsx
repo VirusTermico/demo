f27e0acba31d1788f5f7c5ebe58a3db516500330
Beno <benofernandes27@gmail.com>
2024-10-27 06:30:17 +0100

    change auth mode

--- App.tsx
+++ App.tsx
@@ -1,15 +1,22 @@
-import { View, Text, SafeAreaView, Button } from 'react-native'
-import React from 'react'
-import auth from '@react-native-firebase/auth'
+import {View, Text, SafeAreaView, Button} from 'react-native';
+import React from 'react';
+import auth from '@react-native-firebase/auth';
 const App = () => {
   return (
     <SafeAreaView>
-      <Text style={{color:"#fff"}}>App</Text>
-      <Button title='Login' onPress={()=>{
-          auth().signInAnonymously();
-      }}/>
+      <Text style={{color: '#fff'}}>App</Text>
+      <Button
+        title="Login"
+        onPress={() => {
+          auth()
+            .createUserWithEmailAndPassword('beno@gmail.com', 'cece24Beno')
+            .then(t => {
+              console.log(t.user);
+            });
+        }}
+      />
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default App
+export default App;
